d5256b862a3cab019714a7ad8ad10c135015cd9a
ngoanhquy <ngoanhquy021103@gmai.com>
2024-10-01 17:50:10 +0700

    done

--- EX01/app/src/main/java/com/example/total/MainActivity.java
+++ EX01/app/src/main/java/com/example/total/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.total;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText edtA, edtB, edtKq;
+    Button btnTong;
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKq = findViewById(R.id.edtKq);
+        btnTong = findViewById(R.id.btnTong);
+
+        btnTong.setOnClickListener(view -> {
+            int a = Integer.parseInt(edtA.getText().toString());
+            int b = Integer.parseInt(edtB.getText().toString());
+            int c = a +b;
+            edtKq.setText(c+"");
         });
     }
 }
