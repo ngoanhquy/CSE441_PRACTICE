169016ac2146ba1b540fc21152f36072a5b188d7
ngoanhquy <ngoanhquy021103@gmai.com>
2024-10-01 23:59:34 +0700

    create Gui

--- EX04/app/src/main/java/com/example/bmi/MainActivity.java
+++ EX04/app/src/main/java/com/example/bmi/MainActivity.java
@@ -1,24 +1,56 @@
 package com.example.bmi;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    EditText txtFar, txtCel;
+    Button btnFar, btnCel, btnClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        txtFar = findViewById(R.id.txtFar);
+        txtCel = findViewById(R.id.txtCel);
+        btnFar = findViewById(R.id.btnFar);
+        btnCel = findViewById(R.id.btnCel);
+        btnClear = findViewById(R.id.btnClear);
+
+        btnFar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String far = txtFar.getText().toString();
+                double F = Double.parseDouble(far);
+                txtCel.setText(dcf.format((F - 32) / 1.8));
+            }
+        });
+
+        btnCel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String cel = txtCel.getText().toString();
+                double C = Double.parseDouble(cel);
+                txtFar.setText(dcf.format(C * 1.8 + 32));
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtFar.setText("");
+                txtCel.setText("");
+            }
         });
     }
 }
+
+
+
