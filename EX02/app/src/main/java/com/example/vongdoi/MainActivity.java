9bfa00cd009d52ea86b4669f4f24876827911681
ngoanhquy <ngoanhquy021103@gmai.com>
2024-10-09 16:22:25 +0700

    done

--- EX02/app/src/main/java/com/example/vongdoi/MainActivity.java
+++ EX02/app/src/main/java/com/example/vongdoi/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent1);
         });
     }
-    //Dotrang
     @Override
     protected void onDestroy() {
         super.onDestroy();
