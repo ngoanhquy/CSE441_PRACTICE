b26a27eea3a919b725d19dbb5e73f80ce830fc31
ngoanhquy <ngoanhquy021103@gmai.com>
2024-10-10 15:03:44 +0700

    done

--- EX05/Ex05_1/app/src/main/java/com/example/convertcalendaryear/MainActivity.java
+++ EX05/Ex05_1/app/src/main/java/com/example/convertcalendaryear/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.convertcalendaryear;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,97 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnChange = findViewById(R.id.btn_change);
+        EditText edtDuongLich = findViewById(R.id.edt_duong_lich);
+        EditText edtAmLich = findViewById(R.id.edt_am_lich);
+
+        btnChange.setOnClickListener(view -> {
+            String DuongLich = edtDuongLich.getText().toString();
+            if(!DuongLich.isEmpty()){
+                int NamDuong = Integer.parseInt(DuongLich);
+                String NamAm = ConvertToLunarYear(NamDuong);
+                edtAmLich.setText(NamAm);
+            }
+        });
     }
+
+    private String ConvertToLunarYear(int namDuong) {
+        String can = "";
+        String chi = "";
+
+        switch (namDuong % 10){
+            case 0:
+                can = "Canh";
+                break;
+            case 1:
+                can = "Tân";
+                break;
+            case 2:
+                can = "Nhâm";
+                break;
+            case 3:
+                can = "Quý";
+                break;
+            case 4:
+                can = "Giáp";
+                break;
+            case 5:
+                can = "Ất";
+                break;
+            case 6:
+                can = "Bính";
+                break;
+            case 7:
+                can = "Đinh";
+                break;
+            case 8:
+                can = "Mậu";
+                break;
+            case 9:
+                can = "Kỷ";
+                break;
+        }
+
+        switch (namDuong % 12) {
+            case 0:
+                chi = "Thân";
+                break;
+            case 1:
+                chi = "Dậu";
+                break;
+            case 2:
+                chi = "Tuất";
+                break;
+            case 3:
+                chi = "Hợi";
+                break;
+            case 4:
+                chi = "Tý";
+                break;
+            case 5:
+                chi = "Sửu";
+                break;
+            case 6:
+                chi = "Dần";
+                break;
+            case 7:
+                chi = "Mão";
+                break;
+            case 8:
+                chi = "Thìn";
+                break;
+            case 9:
+                chi = "Tỵ";
+                break;
+            case 10:
+                chi = "Ngọ";
+                break;
+            case 11:
+                chi = "Mùi";
+                break;
+        }
+        return can + " " + chi;
+    }
+
 }
