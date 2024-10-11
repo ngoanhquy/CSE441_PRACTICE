c4bbcf77c60f4fa298bfdb6bcb1de706a538459a
ngoanhquy <ngoanhquy021103@gmai.com>
2024-10-11 14:11:40 +0700

    done

--- EX07/Ex07_1/app/src/main/java/com/example/intent1/MainActivity.java
+++ EX07/Ex07_1/app/src/main/java/com/example/intent1/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.intent1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btn1 = findViewById(R.id.btnNext);
+        btn1.setOnClickListener(view -> {
+            Intent intent1 = new Intent(MainActivity.this,ChildActivity.class);
+            startActivity(intent1);
         });
     }
 }
