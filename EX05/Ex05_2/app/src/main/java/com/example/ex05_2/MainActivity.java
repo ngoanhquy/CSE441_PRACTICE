0a0d2964dd593d9b8e4f8d4627f6e152f642c9d9
ngoanhquy <ngoanhquy021103@gmai.com>
2024-10-11 14:42:50 +0700

    done

--- EX05/Ex05_2/app/src/main/java/com/example/ex05_2/MainActivity.java
+++ EX05/Ex05_2/app/src/main/java/com/example/ex05_2/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.ex05_2;
 
 import android.os.Bundle;
-
 import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
