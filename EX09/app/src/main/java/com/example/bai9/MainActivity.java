6be83c2ccd9773fdd2c220d6dbfafbca66778fe2
ngoanhquy <ngoanhquy021103@gmai.com>
2024-10-15 13:49:36 +0700

    done

--- EX09/app/src/main/java/com/example/bai9/MainActivity.java
+++ EX09/app/src/main/java/com/example/bai9/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.bai9;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton btn_play, btn_stop;
+    Boolean flag = true;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btn_play = findViewById(R.id.btn_play);
+        btn_stop = findViewById(R.id.btn_stop);
+        btn_play.setOnClickListener(view -> {
+            Intent intent1 = new Intent(MainActivity.this, Myservice.class);
+            startService(intent1);
+            if (flag) {
+                btn_play.setImageResource(R.drawable.stop);
+                flag = false;
+            } else {
+                btn_play.setImageResource(R.drawable.play);
+                flag = true;
+            }
+        });
+        btn_stop.setOnClickListener(view -> {
+            Intent intent2 = new Intent(MainActivity.this, Myservice.class);
+            stopService(intent2);
+            btn_play.setImageResource(R.drawable.play);
         });
     }
 }
