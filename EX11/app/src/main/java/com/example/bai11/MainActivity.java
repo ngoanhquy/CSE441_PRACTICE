3719de232022721421306b4452fc2cc6f40d1ad4
ngoanhquy <ngoanhquy021103@gmai.com>
2024-10-15 15:28:53 +0700

    done

--- EX11/app/src/main/java/com/example/bai11/MainActivity.java
+++ EX11/app/src/main/java/com/example/bai11/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.bai11;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnopen;
+    EditText edtlink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtlink=findViewById(R.id.edtLink);
+        btnopen=findViewById(R.id.btnOpen);
+        btnopen.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://"+edtlink.getText().toString()));
+            startActivity(intent);
         });
     }
 }
