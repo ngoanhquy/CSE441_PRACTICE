7618cfa9b468d109396776b566b5f9a7398a34d3
ngoanhquy <ngoanhquy021103@gmai.com>
2024-10-17 02:22:45 +0700

    done

---
+++ EX13/Ex13_Custom_GridView/app/src/main/java/com/example/bai13_custom_gridview/Image.java
@@ -0,0 +1,30 @@
+package com.example.bai13_custom_gridview;
+
+public class Image {
+    private int img;
+    private String name;
+
+    public Image(String name, int img) {
+        this.name = name;
+        this.img = img;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Image(){
+
+    }
+}
