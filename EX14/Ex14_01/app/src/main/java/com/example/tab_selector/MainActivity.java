788a95fe0d4b242db85f2e11465b0505f5e65e61
ngoanhquy <ngoanhquy021103@gmai.com>
2024-10-17 16:31:51 +0700

    done

--- EX14/Ex14_01/app/src/main/java/com/example/tab_selector/MainActivity.java
+++ EX14/Ex14_01/app/src/main/java/com/example/tab_selector/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.tab_selector;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    EditText edta, edtb;
+    Button btnCong;
+    ListView lv1;
+    ArrayList<String> list;
+    ArrayAdapter<String> myarray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        addControl();
+        addEvent();
+    }
+
+    private void addControl() {
+    }
+
+    private void addEvent() {
+        btnCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                XuLyCong();
+            }
+
+            private void XuLyCong() {
+                int a = Integer.parseInt(edta.getText().toString());
+                int b = Integer.parseInt(edtb.getText().toString());
+                String c = a + " + " + b + " = " + (a + b);
+                list.add(c);
+                myarray.notifyDataSetChanged();
+                edta.setText("");
+                edtb.setText("");
+            }
         });
     }
 }
